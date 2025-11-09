5b118312a7f1cbbbac7f834321f0dcce98dfba31
dariu <darius.studiosuipod@gmail.com>
2025-11-08 20:02:20 -0600

    Mini Project component started

--- src/components/MiniProjects/MiniProjects.jsx
+++ src/components/MiniProjects/MiniProjects.jsx
@@ -1,6 +1,7 @@
 import "./MiniProjects.css";
 import label from "../../assets/miniProjects.png";
 
+import Peephole from "../Peephole/Peephole";
 import Planetary from "../Planetary/Planetary";
 import FishHen from "../FishHen/FishHen";
 import Famlet from "../Famlet/Famlet";
@@ -9,12 +10,16 @@ function MiniProject() {
   return (
     <div className="mini_projects">
       <img src={label} alt="" className="mini_projects__label" />
-      <h2 className="mini_projects__title"></h2>
-      <p className="mini_projects__body">
+      <h2 className="mini_projects__title">
+        These are the smaller projects I want to develop before I start on the
+        larger ones
+      </h2>
+      <div className="mini_projects__body">
+        <Peephole />
         <Planetary />
         <FishHen />
         <Famlet />
-      </p>
+      </div>
     </div>
   );
 }
