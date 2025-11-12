a1a08b43d81cc37cae4184be5ac8624669e5fd9f
dariu <darius.studiosuipod@gmail.com>
2025-11-12 10:25:05 -0600

    fix: Header component structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,13 @@ import "./Header.css";
 import { Link } from "react-router-dom";
 
 import aboutLabel from "../../assets/about.png";
-
+import aboutLabelHover from "../../assets/about_hover.png";
+import miniProjects from "../../assets/miniProjects.png";
+import miniProjectsHover from "../../assets/miniProjects_hover.png";
+import lightbyte from "../../assets/lightbyte.png";
+import lightbyteHover from "../../assets/lightbyte_hover.png";
 import name from "../../assets/name.png";
 import banner from "../../assets/banner.png";
-
-import miniProjects from "../../assets/miniProjects.png";
-import lightbyte from "../../assets/lightbyte.png";
 
 function Header() {
   return (
@@ -17,7 +18,16 @@ function Header() {
           <div className="header__left_logo" />
         </Link>
         <Link to="/about">
-          <img src={aboutLabel} alt="" className="header__about-label" />
+          <img
+            src={aboutLabel}
+            alt="About"
+            className="header__about-label header__about-label--default"
+          />
+          <img
+            src={aboutLabelHover}
+            alt="About hover"
+            className="header__about-label header__about-label--hover"
+          />
         </Link>
       </div>
       <div className="header__center">
@@ -29,12 +39,26 @@ function Header() {
         <Link to="/mini-projects">
           <img
             src={miniProjects}
-            alt=""
-            className="header__right_mini-projects"
+            alt="Mini Projects"
+            className="header__right_mini-projects header__right_mini-projects--default"
+          />
+          <img
+            src={miniProjectsHover}
+            alt="Mini Projects hover"
+            className="header__right_mini-projects header__right_mini-projects--hover"
           />
         </Link>
         <Link to="/lightbyte">
-          <img src={lightbyte} alt="" className="header__right_lightbyte" />
+          <img
+            src={lightbyte}
+            alt="Lightbyte"
+            className="header__right_lightbyte header__right_lightbyte--default"
+          />
+          <img
+            src={lightbyteHover}
+            alt="Lightbyte hover"
+            className="header__right_lightbyte header__right_lightbyte--hover"
+          />
         </Link>
       </div>
     </header>
